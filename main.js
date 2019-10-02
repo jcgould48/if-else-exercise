f1061ee141a4ba1a52e306d31fd72bd17b6f8218
Jesse Gould <jesse.gould@codeimmersives.com>
2019-10-02 08:04:13 -0400

    most of the way through

--- main.js
+++ main.js
@@ -1,22 +1,45 @@
-function isItLong() {
-
+function isItLong(sizeOfStr) {
+  if (sizeOfStr.length > 20){
+ return "That's a long string!";
+  } 
 }
 
-function isItMedium() {
-
+function isItMedium(sizeOfStr1) {
+  if ((sizeOfStr1.length >= 10) && (sizeOfStr1.length <= 20)){
+    return "That's a regular sized string!";
+     } 
 }
 
-function isItShort() {
-
+function isItShort(sizeOfStr3) {
+if (sizeOfStr3.length < 10){
+  return 'That\'s a small string!';
+}
 }
 
-function howLongIsMyString() {
-
+function howLongIsMyString(sizeOfStr4) {
+const stringLength = sizeOfStr4.length;
+if (stringLength > 20){
+  return "That's a long string!";
+   } 
+else if ((stringLength >= 10) && (stringLength <= 20)){
+    return "That's a regular sized string!";
+     } 
+else if(stringLength < 10){
+  return 'That\'s a small string!';
+}
 }
 
-function instructorHeight() {
+function instructorHeight(nme) {
+  if (nme==='Colin'){
+    return 62;
+  }
+  else if (nme === 'Mesuara'){
+    return 67;
+  }
+  {
+  return 'I don\'t know that instructor!';}
+  }
 
-}
 
 
 
